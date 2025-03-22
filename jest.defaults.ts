30d2acf01b6efaa1fb2bbc78c3dbd0d7da1ee030
Julian Lasso <julian.lasso@sofka.com.co>
2025-03-22 15:50:42 -0500

    Agregar mapeo de módulos y configuraciones para las capas de dominio, aplicación e infraestructura en Jest y TypeScript

--- jest.defaults.ts
+++ jest.defaults.ts
@@ -9,6 +9,9 @@ export const defaults: Config = {
   },
   moduleNameMapper: {
     '^@common/(.*)$': '<rootDir>/src/common/$1',
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@application/(.*)$': '<rootDir>/src/application/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
   },
   testTimeout: 30000,
   verbose: true,
